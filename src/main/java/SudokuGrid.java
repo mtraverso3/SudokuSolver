47bd87878a9321c7876fde21d4caeb3191ebc648
Marcos Traverso <marcostraverso2003@gmail.com>
2020-03-24 02:16:28 -0700

    Added Constructor and toString override

--- src/main/java/SudokuGrid.java
+++ src/main/java/SudokuGrid.java
@@ -1,9 +1,28 @@
+import java.util.Arrays;
+
 public class SudokuGrid
 {
-    public int[][] grid;
+    public Cell[][] grid = new Cell[9][9];
 
-    SudokuGrid(){
-
+    SudokuGrid(int[][] vals){
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                this.grid[row][col] = new Cell(vals[row][col],false);
+            }
+        }
     }
 
+    @Override
+    public String toString()
+    {
+        StringBuilder a = new StringBuilder();
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                a.append(grid[row][col].getValue()).append(" ");
+            }
+            a.append("\n");
+        }
+
+        return a.toString();
+    }
 }
