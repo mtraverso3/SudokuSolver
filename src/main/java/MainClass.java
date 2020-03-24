8fa101f19635be84e205a13a879ce911ee3557fd
Marcos Traverso <marcostraverso2003@gmail.com>
2020-03-24 02:18:05 -0700

    Converted to declaration

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,7 +2,10 @@ public class MainClass
 {
     public static void main(String[] args)
     {
-        new GridLayoutDemo();
+        SudokuFrame sudoku = new SudokuFrame();
+        SudokuGrid startGrid = sudoku.getSudokuGrid();
 
+        //option 1: have program wait until user submits to continue here.
+        //option 2: automatically call solve the moment submit is pressed.
     }
 }
